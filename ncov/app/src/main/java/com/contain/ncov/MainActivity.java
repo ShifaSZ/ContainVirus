743b04879290728abc3368f69f6c2e76ce243c0b
Shifa Zhang <zhangshifa07504@gmail.com>
2020-02-05 00:05:01 -0500

    Removed unused files

--- ncov/app/src/main/java/com/contain/ncov/MainActivity.java
+++ ncov/app/src/main/java/com/contain/ncov/MainActivity.java
@@ -171,10 +171,6 @@ public class MainActivity extends AppCompatActivity {
     public class MyLocationListener extends BDAbstractLocationListener{
         @Override
         public void onReceiveLocation(BDLocation location){
-            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
-            //以下只列举部分获取经纬度相关（常用）的结果信息
-            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
-
             double latitude = location.getLatitude();    //获取纬度信息
             double longitude = location.getLongitude();    //获取经度信息
             float radius = location.getRadius();    //获取定位精度，默认值为0.0f
